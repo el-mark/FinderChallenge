561d6b795416e66f2e01a6e5bfe3897bdfc21779
Mark <mharmsenr@gmail.com>
2017-04-07 23:04:51 -0500

    Autocompletar busqueda

--- src/scripts/search-form.js
+++ src/scripts/search-form.js
@@ -3,6 +3,16 @@ function searchForm(){
 }
 
 
+
+var input = document.getElementById("search-input");
+new Awesomplete(input, {
+	list: ["Poema de Gilgamesh", "Las mil y una noches", "Todo se desmorona", "Cuentos infantiles", "Divina Comedia", "Orgullo y prejuicio", "Viaje al fin de la noche"],
+	minChars: 3,
+	maxItems: 7,
+});
+
+
+
 $(document).ready(function(){
     $('#search-button').attr('disabled',true);
     $('#search-input').keyup(function(){
