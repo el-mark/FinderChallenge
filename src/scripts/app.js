9648d2857c5b388414f47c8ac7cc5f6542b07a42
Mark <mharmsenr@gmail.com>
2017-04-07 23:42:26 -0500

    Listar las búsquedas guardadas por defecto v1

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,3 +3,19 @@
 */
 
 searchForm();
+
+function printData(data){
+    console.log(data.entities);
+
+    for (var key in data.entities) {
+    	$( ".saved-searches-div" ).append('<h2>' + key + '</h2>');
+    }
+}
+
+
+$.ajax({
+    url: "books-schema.json",
+    success: function (data) {
+    	printData(data);
+    }
+});
